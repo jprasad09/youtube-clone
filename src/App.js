f252039764ca8e6d74a56078cff611c4fa9cf075
jprasad09 <phjadhav09@gmail.com>
2022-01-09 10:38:58 +0530

    created folder structure and added some components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
-function App() {
+import './_app.scss';
+import { Container } from 'react-bootstrap';
+import Header from './components/header/Header';
+import Sidebar from './components/sidebar/Sidebar';
+import HomeScreen from './screens/homeScreen/HomeScreen';
+import { useState } from 'react';
+
+const App = () => {
+
+
+  const [sidebar, toggleSidebar] = useState(false);
+
+  const handleToggleSidebar = () => toggleSidebar(value => !value)
+
   return (
-    <div>
-      youtube clone
-    </div>
+    <>
+      <Header handleToggleSidebar={handleToggleSidebar}/>
+      <div className="app__container">
+        <Sidebar sidebar={sidebar} handleToggleSidebar={handleToggleSidebar}/>
+        <Container fluid className='app__main'>
+          <HomeScreen />
+        </Container>
+      </div>
+    </>
   );
 }
 
---
+++ src/components/categoriesBar/CatrgoriesBar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './_categoriesBar.scss';
+
+const CatrgoriesBar = () => {
+    return (
+        <div>
+            CategoriesBar
+        </div>
+    )
+}
+
+export default CatrgoriesBar;
